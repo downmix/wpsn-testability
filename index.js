00819d2390437e1f58a1fb534885dfde9c3be384
downmix <downmix@downmix.net>
2017-09-28 16:16:23 +0900

    bugsnap add

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ require('./env')
 
 const createApp = require('./src/app')
 const slack = require('./src/slack')
+const bugsnag = require("bugsnag");
+bugsnag.register("dd50d5208208d5f4cbde7b3bf08bac8b");
 
 const app = createApp({postMessage: slack.postMessage})
+app.use(bugsnag.requestHandler);
+app.use(bugsnag.errorHandler);
+
 
 const PORT = process.env.PORT || 3000
 
